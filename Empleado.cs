0a9e912d0cf7ebe4d54334ff142b6d041b1f8cad
miguelangelrp-ux <angelmiguelrp9@gmail.com>
2025-12-01 08:44:33 -0400

    Variable cambiada  y creando constructor

--- Empleado.cs
+++ Empleado.cs
@@ -7,6 +7,20 @@ namespace Sistema_Nomina
 {
     public class Empleado
     {
+        public string nombre {get; set;}
+        public string codigo {get; set;}
+        public string departamento {get; set;}
+        public double salarioBase {get; set;}
         
+
+        //Constructor
+        public Empleado (string Nombre, string Codigo, string Departamento, double SalarioBase)
+        {
+            nombre = Nombre;
+            codigo = Codigo;
+            departamento = Departamento;
+            salarioBase = SalarioBase;
+        }
+
     }
 }
